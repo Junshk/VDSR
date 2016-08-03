72036b8590653a1535f22796c270f839c70f6290
Junshk <snu11ee@snu.ac.kr>
2016-08-03 15:40:13 +0900

    Update net_.lua

--- net_.lua
+++ net_.lua
@@ -5,41 +5,8 @@ function net_return(net_type)
 
 local net, criterion
 
-
-if net_type == 'srnet' then
-require 'Dropbox/SR/SRNet'
-
-net , criterion = SRResnet(layer)
-elseif net_type == 'botsrnet' then
-require 'Dropbox/SR/SRNet'
-
-net, criterion = BotRBU(layer)
-
-elseif net_type == 'botRUnet' then
-require 'Dropbox/SR/SRNet'
-
-net, criterion = BotRU(layer)
-
-
-elseif net_type == 'MulNet' then
-
-require 'Dropbox/MulSR/mulNet'
-
-net,criterion = MulNet(layer)
-elseif net_type == 'zoom' then
-
-require 'Dropbox/MulSR/ZoomNet' 
-require 'Dropbox/MulSR/zoomparam'
-net,criterion = ZoomNet(capNum,eachlayer,zoom,adjConst)
-
-elseif net_type == 'amp' then
-require 'SuperResolution/AmpNet'
-require 'ampOpt'
-print(option1)
-net,criterion = ampNet(layer,option1,option2)
-
-elseif net_type == 'VDSR' then
-require 'SuperResolution/VDSR'
+if net_type == 'VDSR' then
+require 'VDSR'
 require 'VDSRopt'
 net = VDSR(20,option)
 
